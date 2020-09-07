6ba857fcd26a781c80efe7d22aafcc14ea2269b9
Qaifi Khan <qaifikhan@gmail.com>
2020-09-07 17:58:46 +0530

    first commit

---
+++ src/VideoCard/VideoCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import classes from './VideoCard.module.css';
+
+const VideoCard = (props) => {
+
+    return(
+        <Link className={classes.VideoCard} to={`/video/watch/${props.id}`}>
+            <div>
+                <img className={classes.Thumbnail} src={props.thumbnail} alt="Video Thumbnail" />
+                <h3 className={classes.Title}>{props.title}</h3>
+            </div>
+        </Link>
+    );
+}
+
+export default VideoCard;
